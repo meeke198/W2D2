5cc46bb60a8be0e1f4af05f3b16d2dcc84ea5d51
hien nguyen <thuhiennguyen198@gmail.com>
2021-08-17 14:17:42 -0400

    Wrap up

--- hangman_project/lib/hangman.rb
+++ hangman_project/lib/hangman.rb
@@ -1,38 +1,53 @@
 class Hangman
-  DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
-  attr_reader :guess_word, :attempted_chars, :remaining_incorrect_guesses
-  def self.random_word
-    DICTIONARY.sample
-  end
+    attr_reader :attempted_chars, :guess_word, :remaining_incorrect_guesses
+    DICTIONARY = ["cat", "dog", "rabbit", "snake", "lion", "elephant"]
+    
+    def self.random_word
+        DICTIONARY.sample
+    end
+    def initialize
+        @secret_word = Hangman.random_word
+        @guess_word = Array.new(@secret_word.length, "_")
+        @attempted_chars = []
+        @remaining_incorrect_guesses = 5
 
-  def initialize
-    @secret_word = Hangman.random_word
-    @guess_word = Array.new(@secret_word.length, "_")
-    @attempted_chars = []
-    @remaining_incorrect_guesses = 5
-  end
-
-  def already_attempted?(char)
-    @attempted_chars.include?(char)
-  end
-
-  def get_matching_indices(char)
-    indices = []
-    @secret_word.each_char.with_index do |sec_char, i|
-      if sec_char == char
-        indices << i
-      end
     end 
-    indices
-  end
 
-  def fill_indices(char, indices)
-    indices.each {|i| @guess_word[i] = char}
-  end
+    def already_attempted?(char)
+        attempted_chars.include?(char) ? true : false
+    end 
+
+    def get_matching_indices(char)
+        indices = []
+        if @secret_word.include?(char)
+            @secret_word.each_char.with_index {|letter, i| indices << i if char == letter }
+        end
+        indices
+    end
+
+    def fill_indices(char, indices)
+        indices.each {|i| @guess_word[i] = char}
+    end
+
+    def try_guess(char)
+        if self.already_attempted?(char)
+            print "That has already been attempted"
+            return false
+        else
+            if self.get_matching_indices(char).empty?
+                @attempted_chars << char
+                @remaining_incorrect_guesses -= 1
+                return true
+            else 
+                indices = self.get_matching_indices(char)
+                self.fill_indices(char, indices)
+            end
+        end
+    end
 
 
-  def try_guess(char)
-    if self.already_attempted?(char) == true
+ def try_guess(char)
+    if self.already_attempted?(char)
       puts 'that has already been attempted'  
       return false
     end
@@ -53,31 +68,36 @@ class Hangman
     self.try_guess(char)
   end
 
-  def win?
-    if @secret_word == @guess_word.join("")
-      print "WIN! AMAZING!!!! "
-      return true
-    else 
-      return false
+  def ask_user_for_guess
+    print 'Enter a char:' 
+    char = gets.chomp
+    try_guess(char)
+  end 
+
+    def win?
+      if @guess_word.join("") == @secret_word
+          print 'WIN'
+          return true
+      else
+        return false
+      end
     end
-  end
 
-  def lose?
-  if @remaining_incorrect_guesses == 0
-    print "OOPS! YOU LOSE :(! TRY IT AGAIN :) "
-    return true
-  else
-    return false
-  end
-  end
-
-  def game_over?
-    if win? || lose?
-      print "The secret word is " + @secret_word
-      return true
-    else
-      false
+    def lose?
+      if @remaining_incorrect_guesses == 0
+          print 'LOSE'
+          return true
+      else
+        return false
+      end
     end
-  end
 
+    def game_over?
+      if win? || lose?
+          print "The secret word is #{@secret_word}"
+          return true
+      else
+          return false
+      end
+    end
 end
