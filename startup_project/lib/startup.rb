3432c86e14989e6601429d985ba833049e4f4077
hien nguyen <thuhiennguyen198@gmail.com>
2021-08-18 09:56:45 -0400

    Finished

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -1,7 +1,6 @@
-require "employee"
-
+require_relative "employee"
 class Startup
-attr_reader :name, :funding, :salaries, :employees
+    attr_reader :name, :funding, :salaries, :employees
     def initialize(name, funding, salaries)
         @name = name
         @funding = funding
@@ -10,45 +9,43 @@ attr_reader :name, :funding, :salaries, :employees
     end
 
     def valid_title?(title)
-        @salaries.has_key?(title)
+        @salaries.has_key?(title) ? true : false
     end
 
     def >(another_startup)
-        self.funding > another_startup.funding
-    end
+        self.funding > another_startup.funding ? true : false
+    end 
 
     def hire(employee_name, title)
-        if !self.valid_title?(title)
-            raise "Error!! Title does not exist"
-        else 
-           @employees << Employee.new(employee_name, title)
+        if valid_title?(title)
+            @employees << Employee.new(employee_name, title)
+        else
+            raise "Error"
         end
     end
-
     def size
         @employees.length
     end
 
-    def pay_employee(employee)
-        payment = @salaries[employee.title]
-        if @funding >= payment
-            employee.pay(payment)
-            @funding -= payment
+    def pay_employee(employee_instance)
+        employee_salary = @salaries[employee_instance.title]
+        if @funding > employee_salary
+            employee_instance.pay(employee_salary)
+            @funding -= employee_salary
         else
-            raise "Not enough funding"
-        end
+            raise "Out of funding"
+        end 
     end
 
     def payday
-        @employees.each {|employee| self.pay_employee(employee)}
+        @employees.map {|employee| self.pay_employee(employee)}
     end
 
     def average_salary
         sum = 0
-        @employees.each {|employee| sum += @salaries[employee.title]} # why this didn't work @salaries.values.sum / @employees.length
-         sum / @employees.length
-    end
-
+        @employees.each {|employee| sum += @salaries[employee.title]}
+        sum / @employees.length
+    end 
     def close
         @employees = []
         @funding = 0
@@ -56,12 +53,16 @@ attr_reader :name, :funding, :salaries, :employees
 
     def acquire(another_startup)
         @funding += another_startup.funding
-        another_startup.salaries.each do|title, salary|
+        another_startup.salaries.each do |title, salary|
             if !@salaries.has_key?(title)
                 @salaries[title] = salary
             end
         end
-        @employees += another_startup.employees 
+        @employees += another_startup.employees
         another_startup.close
     end
 end
+
+
+# chu y line 242 rspec: should add the given startup's salaries to our @salaries, without overwriting any of our existing @salaries" do
+# nghia là chỉ add những title và salary mà current startup chưa có, nếu có rồi thì khỏi add, để trả lương cho chức vụ đó (bên another_startup) theo như chính sachh của current startup, vì add nó sẽ overwrite và thay đổi theo another_startup. 
